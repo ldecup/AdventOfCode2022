26a4881cd78ee665b0857c121f9454d3b5a85394
ldecup <posedeus@hotmail.fr>
2023-09-01 20:29:41 +0200

    Day11 part 1

--- Day11/main.py
+++ Day11/main.py
@@ -1,7 +1,89 @@
-inFile = open('Day11/testinput.txt', 'r')
+import math
+import os
+
+inFile = open('Day11/input.txt', 'r')
 inData = inFile.readlines()
 inFile.close()
 
 data = []
 for line in inData:
-    data.append(line.rstrip('\n'))
+    data.append(line.rstrip('\n').lstrip(' ').split(':'))
+data.append([''])
+
+class monkey:
+    def __init__(self, id, items, operation, testDivisibleBy, thowToWhenTrue, thowToWhenFalse, inspectedItems):
+        self.id = id
+        self.items = items
+        self.operation = operation
+        self.testDivisibleBy = testDivisibleBy
+        self.thowToWhenTrue = thowToWhenTrue
+        self.thowToWhenFalse = thowToWhenFalse
+        self.inspectedItems = inspectedItems
+
+monkeyList = []
+currentMonkey = monkey(None,None,None,None,None,None,None)
+for line in data:
+    if line[0].split(' ')[0] == 'Monkey':
+        currentMonkey.id = line[0].split(' ')[1]
+    elif line[0] == 'Starting items':
+        currentMonkey.items = line[1].lstrip(' ').split(', ')
+    elif line[0] == 'Operation':
+        currentMonkey.operation = line[1].lstrip(' ').split(' ')[-2:]
+    elif line[0] == 'Test':
+        currentMonkey.testDivisibleBy = int(line[1].lstrip(' ').split(' ')[2])
+    elif line[0] == 'If true':
+        currentMonkey.thowToWhenTrue = int(line[1].lstrip(' ').split(' ')[3])
+    elif line[0] == 'If false':
+        currentMonkey.thowToWhenFalse = int(line[1].lstrip(' ').split(' ')[3])
+    elif line[0] == '':
+        currentMonkey.inspectedItems = 0
+        itemList = []
+        for item in currentMonkey.items:
+            itemList.append(int(item))
+        currentMonkey.items = itemList
+        monkeyList.append(currentMonkey)
+        currentMonkey = monkey(None,None,None,None,None,None,None)
+
+
+
+
+
+
+
+for i in range(10000): # <- homme naïf
+    os.system('cls')
+    print("Round: " + str(i))
+    for monkey in monkeyList:
+        for item in monkey.items:
+            monkey.inspectedItems += 1
+            #Compute worry level
+            if monkey.operation[0] == '*':
+                if monkey.operation[1] == 'old':
+                    worryLevel = item * item
+                else:
+                    worryLevel = item * int(monkey.operation[1])
+            elif monkey.operation[0] == '+':
+                if monkey.operation[1] == 'old':
+                    worryLevel = item + item
+                else:
+                    worryLevel = item + int(monkey.operation[1])
+            #worryLevel = math.floor(worryLevel/3)
+
+            #Perform monkey action
+            if worryLevel % monkey.testDivisibleBy == 0:
+                monkeyList[monkey.thowToWhenTrue].items.append(worryLevel)
+                #print("Monkey " + str(monkey.id) + " threw " + str(worryLevel) + " to monkey " + str(monkey.thowToWhenTrue))
+            else:
+                monkeyList[monkey.thowToWhenFalse].items.append(worryLevel)
+                #print("Monkey " + str(monkey.id) + " threw " + str(worryLevel) + " to monkey " + str(monkey.thowToWhenFalse))
+        monkey.items = []
+
+print("Monkey inventories:")
+monkeyActivityList = []
+for monkey in monkeyList:
+    monkeyActivityList.append(monkey.inspectedItems)
+    print(monkey.items)
+monkeyActivityList = sorted(monkeyActivityList)
+
+monkeyBusiness = monkeyActivityList[-1] * monkeyActivityList[-2]
+print("Monkey business: " + str(monkeyBusiness))
--- README.md
+++ README.md
@@ -3,25 +3,25 @@ Solutions to the puzzles in https://adventofcode.com/2022
 
 Current status:
 
-Day 1 :     **
+Day 1 : **
 
-Day 2 :     **
+Day 2 : **
 
-Day 3 :     **
+Day 3 : **
 
-Day 4 :     **
+Day 4 : **
 
-Day 5 :     **
+Day 5 : **
 
-Day 6 :     **
+Day 6 : **
 
-Day 7 :     **
+Day 7 : **
 
-Day 8 :     **
+Day 8 : **
 
-Day 9 :     
+Day 9 : 
 
-Day 10 :    **
+Day 10 : **
 
 Day 11 : 
 
